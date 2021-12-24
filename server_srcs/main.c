17de7c26436d22645a6f3fefcfb5f46b9db662ec
kkai <kkai@student.42tokyo.jp>
2021-12-24 13:26:21 +0900

    add print_pid

--- server_srcs/main.c
+++ server_srcs/main.c
@@ -6,35 +6,78 @@
 /*   By: kkai <kkai@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/18 15:13:45 by kkai              #+#    #+#             */
-/*   Updated: 2021/12/23 15:45:57 by kkai             ###   ########.fr       */
+/*   Updated: 2021/12/24 13:24:37 by kkai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/server.h"
 
-void	handler(int sig)
+void	print_msg(char *tmp)
+{
+	ft_putendl_fd(tmp, 1);
+	kill(client_pid, SIGUSR1);
+}
+
+void	receive_bit(int bit)
+{
+	static int 	i;
+	static int 	j;
+	static char	msg;
+	static char tmp[5]
+
+	if (i < 8)
+	{
+		msg |= (bit << i);
+		i++;
+	}
+	if(i == 8)
+	{
+		tmp[j] = msg;
+		if (tmp[j] == EOT)
+		{
+			print_msg(tmp);
+		}
+		j++;
+		i = 0;
+		msg = 0;
+	}
+}
+
+void	handler(int sig, siginfo_t *info, void *)
 {
 	if (sig == SIGUSR1)
-		printf("ユーザー１\n, %d\n", sig);
+		receive_bit(1);
+	// printf("ユーザー１\n, %d\n", sig);
 	if (sig == SIGUSR2)
-		printf("ユーザー２\n, %d\n", sig);
+		receive_bit(0);
+	// printf("ユーザー２\n, %d\n", sig);
 }
 void	receive_signal(void)
 {
 	struct sigaction act;
 
-	act.sa_handler = handler;
+
+	ft_bzero(&act, sizeof(struct sigaction));
+	act.sa_sigaction = handler;
 	sigemptyset(&act.sa_mask);
 	// sigaddset(&sa_)
-	// act.sa_flags = SA_SIGINFO;
+	act.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &act, NULL);
 	sigaction(SIGUSR2, &act, NULL);
 }
 // シグナルを受け取る
 //シグナルごとに処理を変える
+
+
+void	print_pid(void)
+{
+	ft_putstr_fd("SERVER'S PID : ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putendl_fd("", 1);
+}
 int main()
 {
-
+	print_pid();
 	receive_signal();
 
 	while (1)
