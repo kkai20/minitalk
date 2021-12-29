7fe2669f7e8b4f139dc13754559ceb2b6129bf08
kkai <kkai@student.42tokyo.jp>
2021-12-29 21:00:47 +0900

    ok

--- client_srcs/sent_text.c
+++ client_srcs/sent_text.c
@@ -6,7 +6,7 @@
 /*   By: kkai <kkai@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 13:46:04 by kkai              #+#    #+#             */
-/*   Updated: 2021/12/29 17:25:18 by kkai             ###   ########.fr       */
+/*   Updated: 2021/12/29 21:00:25 by kkai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,11 +51,9 @@ void	send_text(pid_t server_pid, char *argv)
 
 static void	handler(int sig)
 {
-	if (sig == SIGUSR1)
-	{
-		g_flag = 1;
-		ft_putendl_fd("Success sent message", STDOUT_FILENO);
-	}
+	(int)sig;
+	g_flag = 1;
+	ft_putendl_fd("Success sent message", STDOUT_FILENO);
 }
 
 void	receive_ack(void)
